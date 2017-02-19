0fe88971d82e75c77c05a1d0309a74f9c92a897f
no-PC\no <abdullahwasim42@gmail.com>
2017-02-20 02:31:57 +0600

    ggg

---
+++ basic/practice.js
@@ -0,0 +1,7 @@
+var person=prompt("please enter your name");
+if (person!=null)
+{
+    document.getElementById("demo").innerHTML = "hello" +person + "how are you today?";
+}
+// var response= confirm('press a buttion');
+// alert("alert");
